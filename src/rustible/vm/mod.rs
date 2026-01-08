2ae3407b5d5e4b749798fa3f66f61c59d263b69e
Marcin Grzybek <marcin.grzybek@twoupdigital.com>
2026-01-08 12:33:31 +0100

    add interactive mode to rustible, rewrite unit test to use exec_command function

--- src/rustible/vm/mod.rs
+++ src/rustible/vm/mod.rs
@@ -1,3 +1,4 @@
 //#[cfg(feature = "rustible")]
 pub mod vm_remote;
 pub mod client;
+pub mod models;
--- src/rustible/vm/vm_remote/vm_remote.rs
+++ src/rustible/vm/vm_remote/vm_remote.rs
@@ -1,4 +1,7 @@
-use crate::rustible::vm::client::client::Session;
+use crate::rustible::vm::{
+    client::client::Session,
+    models::Cli};
+use clap::Parser;
 use anyhow::{Ok, Result};
 
 /// Function to interact with remote server
@@ -30,4 +33,35 @@ pub async fn exec_command_on_remote(user: String, ssh_key_path: String, ip_list:
 
 }
 
+/// Function to interact with remote server via cli
+/// Host and Commands are Vec<String>
+/// Additional arguments are separated by ","
+pub async fn exec_command_on_remote_cli() -> Result<()> {
 
+    let cli = Cli::parse();
+
+    let username = cli.username;
+    let private_key = cli.private_key;
+
+    let ip_s = cli.host; 
+    let command_s = cli.command;
+
+    for i in ip_s.iter() {
+        for c in command_s.iter() {
+            println!("{:#?}", i);
+            let mut ssh = Session::connect(
+                private_key.clone(),
+                username.clone(),
+                (i.to_string(), 22),
+                ).await?;
+            let code = ssh
+                .call(
+                    c
+                ).await?;
+            println!("Exitcode: {:?}", code);
+        }
+    }
+
+    Ok(())
+
+}
