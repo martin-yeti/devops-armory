a3c9c28a2537c9d7f4d1c67ac799f62f8ef2c890
Marcin Grzybek <marcin.grzybek@twoupdigital.com>
2026-03-17 10:53:30 +0100

    get deployment/service/ingress - #39

---
+++ src/cloud/gcp/gke/ingress/get.rs
@@ -0,0 +1,67 @@
+use std::time::Duration;
+
+use awc::{
+    Client, Connector,
+};
+
+use openssl::ssl::{
+    SslConnector, 
+    SslMethod, 
+    SslVerifyMode
+};
+
+/// GET GKE ingress
+/// Token, GKE endpoint, namespace need to be provided
+pub async fn get_gke_ingress_info(
+    token: String,
+    gke_cluster_endpoint: String,
+    gke_cluster_namespace: String,
+    gke_cluster_ingress_name: String,
+) -> Result<(), std::io::Error> {
+
+    let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
+    builder.set_verify(SslVerifyMode::NONE);
+    let myconnector = builder.build();
+    let client = Client::builder()
+        .connector(Connector::new().openssl(myconnector))
+        .finish();
+
+    let get_ingress_request = client
+        .get(format!("https://{gke_cluster_endpoint}:443/apis/networking.k8s.io/v1/namespaces/{gke_cluster_namespace}/ingresses/{gke_cluster_ingress_name}"))
+        .bearer_auth(format!("{token}"))
+        .timeout(Duration::from_secs(30))
+        .send()
+        .await
+        .expect("Request GET ingress failed");
+
+    let mut req = get_ingress_request;
+    let req_status = req.status().as_u16();
+    let respone = req.body().await.unwrap_or_default();
+
+    match req_status {
+        200 => {
+            println!("Request has been successfull: Status: {:?}, {:?}", req_status, respone);
+        },
+        201 => {
+            println!("Successfully created service: {:?}", respone);
+        }
+        400 => {
+            println!("Bad Request. Check URL parameters or body: {:?}", respone);
+        },
+        403 => {
+            println!("You don't have access to perform such request: {:?}", respone);
+        }
+        404 => {
+            println!("Requested resource does not exists: {:?}", respone);
+        },
+        409 => {
+            println!("Requested resource already exists! {:?}", respone)
+        }
+        _ => {
+            println!("Request status mismatch. Check response: {:?}", respone);
+        }
+    }
+
+    Ok(())
+    
+}
