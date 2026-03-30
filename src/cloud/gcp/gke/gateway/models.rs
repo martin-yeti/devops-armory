dfe0bfbc88731e67d31081cd515421e48d3fc3ea
Marcin Grzybek <marcin.grzybek@twoupdigital.com>
2026-03-30 10:44:12 +0200

    bump to version 0.6.0

--- src/cloud/gcp/gke/gateway/models.rs
+++ src/cloud/gcp/gke/gateway/models.rs
@@ -2,65 +2,65 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Gateway {
-    kind: String,
-    api_version: String,
-    metadata: Metadata,
-    spec: GatewaySpec,
+    pub kind: String,
+    pub api_version: String,
+    pub metadata: Metadata,
+    pub spec: GatewaySpec,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Metadata {
-    name: String,
-    namespace: String,
+    pub name: String,
+    pub namespace: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GatewaySpec {
-    gateway_class_name: String,
-    listeners: Vec<Listener>,
-    addresses: Vec<Address>,
+    pub gateway_class_name: String,
+    pub listeners: Vec<Listener>,
+    pub addresses: Vec<Address>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Listener {
-    name: String,
-    protocol: String,
-    port: u16,
-    allowed_routes: AllowedRoutes,
-    tls: Tls,
+    pub name: String,
+    pub protocol: String,
+    pub port: u16,
+    pub allowed_routes: AllowedRoutes,
+    pub tls: Tls,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AllowedRoutes {
-    kinds: Vec<RouteKind>,
-    namespaces: NamespaceSelector,
+    pub kinds: Vec<RouteKind>,
+    pub namespaces: NamespaceSelector,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct RouteKind {
-    kind: String,
+    pub kind: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct NamespaceSelector {
-    from: String,
+    pub from: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Tls {
-    mode: String,
-    options: TlsOptions,
+    pub mode: String,
+    pub options: TlsOptions,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TlsOptions {
     #[serde(rename = "networking.gke.io/pre-shared-certs")]
-    pre_shared_certs: String,
+    pub pre_shared_certs: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Address {
-    r#type: String, 
-    value: String,
+    pub r#type: String, 
+    pub value: String,
 }
 
