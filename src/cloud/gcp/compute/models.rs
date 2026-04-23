0999af6db4c8b48ba1aa75d9a14926b19961b441
Marcin Grzybek <marcin.grzybek@twoupdigital.com>
2026-04-23 16:48:30 +0200

    add get gcp vm info function with proper models and result

--- src/cloud/gcp/compute/models.rs
+++ src/cloud/gcp/compute/models.rs
@@ -66,3 +66,114 @@ pub struct ComputeData {
     pub key: String,
     pub value: String
 }
+
+
+// Get instance info models
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct GetVMInstanceInfo {
+    pub kind: String,
+    pub id: String,
+    pub creationTimestamp: String,
+    pub name: String,
+    pub tags: VMTagsGetVM,
+    pub machineType: String,
+    pub status: String,
+    pub zone: String,
+    pub canIpForward: bool,
+    pub networkInterfaces: Vec<VMNetInterfacesGet>,
+    pub disks: Vec<VMDiskGet>,
+    pub metadata: VMMetadataGet,
+    pub selfLink: String,
+    pub scheduling: VMSchedulingGet,
+    pub cpuPlatform: String,
+    pub labelFingerprint: String,
+    pub startRestricted: bool,
+    pub deletionProtection: bool,
+    pub shieldedInstanceConfig: VMSchieldedInstanceConfigGet,
+    pub shieldedInstanceIntegrityPolicy: VMSchieldedInstancePolicyGet,
+    pub fingerprint: String,
+    pub lastStartTimestamp: String,
+    pub resourceStatus: VMResourceStatusGet
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct VMTagsGetVM {
+    pub items: Vec<String>,
+    pub fingerprint: String
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct VMNetInterfacesGet {
+    pub kind: String,
+    pub network: String,
+    pub subnetwork: String,
+    pub networkIP: String,
+    pub name: String,
+    pub fingerprint: String,
+    pub stackType: String
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct VMDiskGet {
+    pub kind: String,
+    pub r#type: String,
+    pub mode: String,
+    pub source: String,
+    pub deviceName: String,
+    pub index: i32,
+    pub boot: bool,
+    pub autoDelete: bool,
+    pub licenses: Vec<String>,
+    pub interface: String,
+    pub diskSizeGb: i64,
+    pub shieldedInstanceInitialState: Vec<VMShieldedInitialState>,
+    pub architecture: String
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct VMShieldedInitialState {
+    pub dbxs: Vec<VMDBXS>
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct VMDBXS {
+    pub content: String,
+    pub fileType: String
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct VMMetadataGet {
+    pub kind: String,
+    pub fingerprint: String
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct VMSchedulingGet {
+    pub onHostMaintenance: String,
+    pub automaticRestart: bool,
+    pub preemptible: bool,
+    pub provisioningModel: String
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct VMSchieldedInstanceConfigGet {
+    pub enableSecureBoot: bool,
+    pub enableVtpm: bool,
+    pub enableIntegrityMonitoring: bool
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct VMSchieldedInstancePolicyGet {
+    pub updateAutoLearnPolicy: bool
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct VMResourceStatusGet {
+    pub effectiveInstanceMetadata: EffectiveInstanceMetadata
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct EffectiveInstanceMetadata {
+    pub vmDnsSettingMetadataValue: String
+}
