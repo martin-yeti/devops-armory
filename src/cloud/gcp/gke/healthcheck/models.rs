de5d02bbca4b89416388926c915a3acc89e944eb
Marcin Grzybek <marcin.grzybek@twoupdigital.com>
2026-05-14 16:34:03 +0200

    #66 - add custom healthcheck config types to GKE healthcheck models

--- src/cloud/gcp/gke/healthcheck/models.rs
+++ src/cloud/gcp/gke/healthcheck/models.rs
@@ -16,18 +16,8 @@ pub struct Metadata {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct HealthCheckPolicySpec {
-    pub default: DefaultHealthCheckConfig,
+    pub default: DefaultPolicy,
     pub targetRef: TargetRef,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct DefaultHealthCheckConfig {
-    pub checkIntervalSec: u32,
-    pub timeoutSec: u32,
-    pub healthyThreshold: u32,
-    pub unhealthyThreshold: u32,
-    pub logConfig: LogConfig,
-    pub config: HealthCheckConfig,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -37,15 +27,13 @@ pub struct LogConfig {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct HealthCheckConfig {
-    pub r#type: String, 
-    //pub httpHealthCheck: HttpHealthCheck,
-    pub tcpHealthCheck: HttpHealthCheck
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct HttpHealthCheck {
-    pub port: u16,
-    pub requestPath: String,
+    #[serde(rename = "type")]
+    pub kind: Option<ProtocolType>,
+    pub http_health_check: Option<HttpHealthCheck>,
+    pub https_health_check: Option<HttpsHealthCheck>,
+    pub grpc_health_check: Option<GrpcHealthCheck>,
+    pub http2_health_check: Option<Http2HealthCheck>,
+    pub tcp_health_check: Option<TcpHealthCheck>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -55,3 +43,87 @@ pub struct TargetRef {
     pub name: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DefaultPolicy {
+    pub check_interval_sec: Option<u32>,
+    pub timeout_sec: Option<u32>,
+    pub healthy_threshold: Option<u32>,
+    pub unhealthy_threshold: Option<u32>,
+    pub log_config: Option<LogConfig>,
+    pub config: Option<HealthCheckConfig>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ProtocolType {
+    HTTP,
+    HTTPS,
+    GRPC,
+    HTTP2,
+    TCP,
+    #[serde(other)]
+    Unknown,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum PortSpecification {
+    USE_FIXED_PORT,
+    USE_NAMED_PORT,
+    USE_SERVING_PORT,
+    #[serde(other)]
+    Unknown,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ProxyHeader {
+    NONE,
+    PROXY_V1,
+    #[serde(other)]
+    Unknown,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HttpHealthCheck {
+    pub port_specification: Option<PortSpecification>,
+    pub port: Option<u32>,
+    pub host: Option<String>,
+    pub request_path: Option<String>,
+    pub response: Option<String>,
+    pub proxy_header: Option<ProxyHeader>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HttpsHealthCheck {
+    pub port_specification: Option<PortSpecification>,
+    pub port: Option<u32>,
+    pub host: Option<String>,
+    pub request_path: Option<String>,
+    pub response: Option<String>,
+    pub proxy_header: Option<ProxyHeader>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GrpcHealthCheck {
+    pub grpc_service_name: Option<String>,
+    pub port_specification: Option<PortSpecification>,
+    pub port: Option<u32>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Http2HealthCheck {
+    pub port_specification: Option<PortSpecification>,
+    pub port: Option<u32>,
+    pub host: Option<String>,
+    pub request_path: Option<String>,
+    pub response: Option<String>,
+    pub proxy_header: Option<ProxyHeader>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TcpHealthCheck {
+    pub port_specification: Option<PortSpecification>,
+    pub port: Option<u32>,
+    pub port_name: Option<String>,
+    pub request: Option<String>,
+    pub response: Option<String>,
+    pub proxy_header: Option<ProxyHeader>,
+}
