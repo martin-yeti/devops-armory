09c7e3a6c7687c7c19da090c165beb108d621454
Marcin Grzybek <marcin.grzybek@twoupdigital.com>
2026-06-09 12:48:18 +0200

    #97 - get regional ip address from GCP cloud

--- src/cloud/gcp/vpc/address/get_regional.rs
+++ src/cloud/gcp/vpc/address/get_regional.rs
@@ -78,3 +78,29 @@ pub async fn get_regional_ip_name(
     Ok(get_ip_request.name)
 
 }
+
+/// Get global external IP address
+/// Project ID, token, ip_name need to be provided
+pub async fn get_regional_ip_address(
+    token: String,
+    project: String,
+    region: String,
+    ip_name: String
+) -> Result<String, std::io::Error> {
+
+    let client = awc::Client::default();
+    let get_ip_request = client
+        .get(format!("https://compute.googleapis.com/compute/v1/projects/{project}/regions/{region}/addresses/{ip_name}"))
+        .bearer_auth(&token)
+        .insert_header(("Content-Type", "application/json"))
+        .timeout(Duration::from_secs(30))
+        .send()
+        .await
+        .expect("Request GET global IP failed")
+        .json::<GetIpAddress>()
+        .await
+        .unwrap_or_default();
+
+    Ok(get_ip_request.address)
+
+}
