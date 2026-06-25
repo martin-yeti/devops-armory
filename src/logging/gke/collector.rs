4866347ae95c9029eeb8d37f72f42396cdd8da3b
Marcin Grzybek <marcin.grzybek@twoupdigital.com>
2026-06-25 15:52:25 +0200

    add get logs from gke pod

---
+++ src/logging/gke/collector.rs
@@ -0,0 +1,42 @@
+use awc::*;
+use openssl::ssl::{
+    SslConnector, 
+    SslMethod, 
+    SslVerifyMode
+};
+
+/// Get logs from GKE pod
+/// Token, cluster endpoint, namespace and pod name need to be provided
+pub async fn gke_log_collector(
+    token: String,
+    gke_cluster_endpoint: String,
+    gke_cluster_namespace: String,
+    pod_name: String
+) -> Result<(),std::io::Error> {
+
+    let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
+    builder.set_verify(SslVerifyMode::NONE);
+
+    let myconnector = builder.build();
+
+    let client = Client::builder()
+        .connector(Connector::new().openssl(myconnector))
+        .finish();
+
+
+    let mut res = client
+        .get(&format!("https://{gke_cluster_endpoint}/api/v1/namespaces/{gke_cluster_namespace}/pods/{pod_name}/log?&tailLines=10&follow&timestamps=true"))   // <- Create request builder
+        .bearer_auth(&token.clone())
+        .insert_header(("Content-Type", "application/json"))
+        .send()
+        .await
+        .expect("Fail to connect to stream");
+
+    
+    let response = res.body().await.unwrap_or_default();
+
+    println!("{:?}", response);
+
+    Ok(())
+
+}
