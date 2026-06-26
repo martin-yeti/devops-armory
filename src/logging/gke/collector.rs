6bfcb05491b1fcb5b2c0d59de6041de5e035379c
Marcin Grzybek <marcin.grzybek@twoupdigital.com>
2026-06-26 11:12:23 +0200

    modify stream into Log struct

--- src/logging/gke/collector.rs
+++ src/logging/gke/collector.rs
@@ -5,7 +5,11 @@ use openssl::ssl::{
     SslVerifyMode
 };
 
-use futures::TryStreamExt;
+use futures::stream::select_all;
+use futures::{StreamExt, TryStreamExt};
+use tuple_conv::*;
+
+use crate::logging::alerts::models::Log;
 
 /// Get logs from GKE pod
 /// Token, cluster endpoint, namespace and pod name need to be provided
@@ -13,7 +17,12 @@ pub async fn gke_log_collector(
     token: String,
     gke_cluster_endpoint: String,
     gke_cluster_namespace: String,
-    pod_name: String
+    pod_name: String,
+    filtered_pod_name: String,
+    project_name: String,
+    project_region: String,
+    gcp_id: String,
+    gke_cluster_region: String,
 ) -> Result<(),std::io::Error> {
 
     let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
@@ -38,8 +47,44 @@ pub async fn gke_log_collector(
     
     streams.push(res);
 
-    for x in streams.into_iter() {
-        println!("{:?}", x)
+    let mut combined_stream = select_all(streams);
+
+    loop {
+        match combined_stream.next().await {
+            Some(chunk) => match chunk {
+                Ok(chunk_bytes) => {
+
+                    let mut host_name = "".to_string();
+                    if pod_name.contains(&filtered_pod_name) {
+                        let gcp_id = project_name.as_str();
+                        let gcp_region = gke_cluster_region.as_str();
+                        let project_name = project_name.as_str();
+                        host_name = pod_name.to_owned();
+                        let chunk_string =
+                            std::str::from_utf8(&chunk_bytes).expect("Non-UTF8 bytes");
+                        let iter = chunk_string.split_once(char::is_whitespace);
+                        let vec = iter.unwrap_or_default().to_vec();
+                        let log = Log {
+                            time: vec.get(0).map(|x| x.to_string()).unwrap_or_default(),
+                            message: vec.get(1).map(|x| x.to_string()).unwrap_or_default(),
+                            host: host_name.to_string(),
+                            google_project_id: gcp_id.to_string(),
+                            region: gcp_region.to_string(),
+                            project_id: project_name.to_string(),
+                        };
+                        println!("{:#?}", log);
+                    }
+                    
+                }
+                Err(err) => {
+                    eprintln!("Failed to read stream chunk: {}", err)
+                }
+            },
+            None => {
+                println!("Nothing to stream. Exiting");
+                break;
+            }
+        }
     }
 
     Ok(())
