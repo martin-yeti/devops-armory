2fb9239bb6bf33bc378513078cc14b436e818d17
Marcin Grzybek <marcin.grzybek@twoupdigital.com>
2026-06-26 14:50:55 +0200

    change hostname substitution in main loop

--- src/logging/gke/collector.rs
+++ src/logging/gke/collector.rs
@@ -37,16 +37,18 @@ pub async fn gke_log_collector(
     let mut streams = Vec::new();
 
     for pod_name in gke_pod_list {
-        let res = client
-            .get(&format!("https://{gke_cluster_endpoint}/api/v1/namespaces/{gke_cluster_namespace}/pods/{pod_name}/log?&tailLines=10&follow&timestamps=true"))   // <- Create request builder
-            .bearer_auth(&token.clone())
-            .insert_header(("Content-Type", "application/json"))
-            .send()
-            .await
-            .expect("Fail to connect to stream")
-            .into_stream();
-        
-        streams.push(res);
+        if pod_name.contains(&gke_pod_phrase) {
+            let res = client
+                .get(&format!("https://{gke_cluster_endpoint}/api/v1/namespaces/{gke_cluster_namespace}/pods/{pod_name}/log?&tailLines=10&follow&timestamps=true"))   // <- Create request builder
+                .bearer_auth(&token.clone())
+                .insert_header(("Content-Type", "application/json"))
+                .send()
+                .await
+                .expect("Fail to connect to stream")
+                .into_stream();
+            
+            streams.push(res);
+        }
     }
 
     let mut combined_stream = select_all(streams);
@@ -57,7 +59,7 @@ pub async fn gke_log_collector(
                 Ok(chunk_bytes) => {
                     let mut host_name = "".to_string();
                     for gke_pod_name in gke_pod_list {
-                        if gke_pod_name.contains(&gke_pod_phrase) {
+                        //if gke_pod_name.contains(&gke_pod_phrase) {
                             let gcp_id = gcp_id.as_str();
                             let gcp_region = gke_cluster_region.as_str();
                             let project_name = project_name.as_str();
@@ -69,13 +71,13 @@ pub async fn gke_log_collector(
                             let log = Log {
                                 time: vec.get(0).map(|x| x.to_string()).unwrap_or_default(),
                                 message: vec.get(1).map(|x| x.to_string()).unwrap_or_default(),
-                                host: host_name.to_string(),
+                                host: gke_pod_name.to_string(),
                                 google_project_id: gcp_id.to_string(),
                                 region: gcp_region.to_string(),
                                 project_id: project_name.to_string(),
                             };
                             println!("{:#?}", log);
-                        }
+                        //}
                     }
                 }
                 Err(err) => {
