bb25fd43c1e14e8daf9a4aeed3b3d907833e4e6d
Marcin Grzybek <marcin.grzybek@twoupdigital.com>
2026-07-24 12:09:28 +0200

    fix collector_db indexes migrations

--- src/logging/gke/collector_db/migrations/2026-07-23-140555-0000_add_indexes/down.sql
+++ src/logging/gke/collector_db/migrations/2026-07-23-140555-0000_add_indexes/down.sql
@@ -1,5 +1,5 @@
 -- This file should undo anything in `up.sql`
 
-DROP INDEX gcp_id_index;
+DROP INDEX IF EXISTS gcp_id_index;
 
-DROP INDEX logs_project_host_idx;
+DROP INDEX IF EXISTS logs_project_host_idx;
--- src/logging/gke/collector_db/migrations/2026-07-23-140555-0000_add_indexes/up.sql
+++ src/logging/gke/collector_db/migrations/2026-07-23-140555-0000_add_indexes/up.sql
@@ -1,5 +1,5 @@
 -- Your SQL goes here
 
-CREATE INDEX gcp_id_index ON logs (google_project_id);
+CREATE INDEX IF NOT EXISTS gcp_id_index ON logs (google_project_id);
 
-CREATE INDEX logs_project_host_idx ON logs (google_project_id, project_id, region, host);
+CREATE INDEX IF NOT EXISTS logs_project_host_idx ON logs (google_project_id, project_id, region, host);
