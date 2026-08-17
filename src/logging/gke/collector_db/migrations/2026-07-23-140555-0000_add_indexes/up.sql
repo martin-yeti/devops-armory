2d519c8e330955199ba547b53aed34ae72a2cd39
Marcin Grzybek <marcin.grzybek@twoupdigital.com>
2026-08-17 15:39:46 +0200

    remove unnecessary indexes in collector_db migrations

--- src/logging/gke/collector_db/migrations/2026-07-23-140555-0000_add_indexes/up.sql
+++ src/logging/gke/collector_db/migrations/2026-07-23-140555-0000_add_indexes/up.sql
@@ -1,5 +1,3 @@
 -- Your SQL goes here
 
-CREATE INDEX IF NOT EXISTS gcp_id_index ON logs (google_project_id);
-
-CREATE INDEX IF NOT EXISTS logs_project_host_idx ON logs (google_project_id, project_id, region, host);
+CREATE INDEX IF NOT EXISTS logs_project_host_idx ON logs (google_project_id, project_id, region, time);
