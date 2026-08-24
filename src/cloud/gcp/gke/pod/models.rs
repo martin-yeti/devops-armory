a60a0ca23ca81d2178f65c5780e1e35a8ec49530
Marcin Grzybek <marcin.grzybek@twoupdigital.com>
2026-08-24 16:24:24 +0200

    add further info about pod resources

--- src/cloud/gcp/gke/pod/models.rs
+++ src/cloud/gcp/gke/pod/models.rs
@@ -24,10 +24,42 @@ pub struct PodName {
 #[derive(Debug, Deserialize, Default, Clone)]
 pub struct ContainerStatuses {
     pub podIP: String,
+    pub startTime: String,
     pub containerStatuses: Vec<Ready>,
 }
 
 #[derive(Debug, Deserialize, Default, Clone)]
 pub struct Ready {
+    pub name: String,
+    pub state: PodState,
     pub ready: bool,
+    pub restartCount: i16,
+    pub image: String,
+    pub imageID: String,
+    pub started: bool,
+    pub allocatedResources: PodAllocatedResources,
+    pub resources: PodResources
 }
+
+#[derive(Debug, Deserialize, Default, Clone)]
+pub struct PodState {
+    pub running: PodStateRunning
+}
+
+#[derive(Debug, Deserialize, Default, Clone)]
+pub struct PodStateRunning {
+    pub startedAt: String
+}
+
+#[derive(Debug, Deserialize, Default, Clone)]
+pub struct PodAllocatedResources {
+    pub cpu: String,
+    pub memory: String
+}
+
+#[derive(Debug, Deserialize, Default, Clone)]
+pub struct PodResources {
+    pub limits: PodAllocatedResources,
+    pub requests: PodAllocatedResources
+}
+
